395485b83edd5ee096fe7b32dd26df19ba787177
Andres-Ortega-lozano <andresortegal522@gmail.com>
2024-09-16 13:21:13 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+    // console.log("test",btn);
+    btn.addEventListener("click", () => {
+        if (btn.id === "=") {
+        display.value = eval(display.value);
+        } else if (btn.id === "AC") {
+            display.value = "";
+        } else if (btn.id == "DEL") {
+            display.value = display.value.slice(0, -1);
+        } else {
+            display.value += btn.id
+        }
+    } );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
